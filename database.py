d379ff9182b8a30f951a75c265d8b3ab0a8a33c9
Miriam Singleton <mpeaster0354@floridapoly.edu>
2024-04-17 23:26:25 -0400

    Fully integrated db / Created webpage for 4riversSmokehouse
    Used django syntax, python, and flask to get the various data items from the database and insert them into a webpage.
    
    Used flask + qrcode to generate a qrcode for the library and saved it in a folder in images for later use.

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3 as sql
+import qrcode
 
 conn = sql.connect('SSEData.db')
 conn.row_factory = sql.Row
@@ -12,6 +13,31 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/4riversSmokehouse')
+def restaurant():
+    qr=qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data('http://127.0.0.1:5000/restaurant')
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="purple", back_color="black")
+    img.save("./static/images/qrcodes/qr4riversSmokehouse.png")
+    conn = sql.connect('SSEData.db')
+    conn.row_factory = sql.Row
+    print("Opened database successfully in the restrictions route")
+    cur = conn.cursor()
+
+    sqlQ = ("""SELECT * FROM LakelandEatsList WHERE rowid=1;""")
+    print(sqlQ)
+    cur.execute(sqlQ)
+    data = cur.fetchall()
+
+    return render_template('4riversSmokehouse.html', data=data)
+
 
 @app.route('/login')
 def login():
