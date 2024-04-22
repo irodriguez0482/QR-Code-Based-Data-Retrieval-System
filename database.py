135770c505d4deea3f6899460f22ae9f88af08c6
Miriam Singleton <mpeaster0354@floridapoly.edu>
2024-04-22 10:09:24 -0400

    Added more restaurant pages w/ qr codes
    Changed background color of qr codes to improve readability.

--- database.py
+++ database.py
@@ -16,16 +16,16 @@ def index():
     return render_template('index.html')
 
 @app.route('/4riversSmokehouse')
-def restaurant():
+def riversSmokehouse():
     qr=qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data('http://127.0.0.1:5000/qr4riversSmokehouse')
+    qr.add_data('http://127.0.0.1:5000/4riversSmokehouse')
     qr.make(fit=True)
-    img = qr.make_image(fill_color="purple", back_color="black")
+    img = qr.make_image(fill_color='#532d8e', back_color="white")
     img.save("./static/images/qrcodes/qr4riversSmokehouse.png")
 
     conn = sql.connect('SSEData.db')
@@ -39,6 +39,57 @@ def restaurant():
     data = cur.fetchall()
 
     return render_template('4riversSmokehouse.html', data=data)
+
+@app.route('/Wakame-sushi')
+def wakameSushi():
+    qr=qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data('http://127.0.0.1:5000/Wakame-sushi')
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='#532d8e', back_color="white")
+    img.save("./static/images/qrcodes/Wakame-sushi.png")
+
+    conn = sql.connect('SSEData.db')
+    conn.row_factory = sql.Row
+    print("Opened database successfully in the restrictions route")
+    cur = conn.cursor()
+
+    sqlQ = ("""SELECT * FROM LakelandEatsList WHERE rowid=66;""")
+    print(sqlQ)
+    cur.execute(sqlQ)
+    data = cur.fetchall()
+
+    return render_template('Wakame-sushi.html', data=data)
+
+@app.route('/abuelos-mexican')
+def abuelosMexican():
+    qr=qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data('http://127.0.0.1:5000/abuelos-mexican')
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='#532d8e', back_color="white")
+    img.save("./static/images/qrcodes/abuelos-mexican.png")
+
+    conn = sql.connect('SSEData.db')
+    conn.row_factory = sql.Row
+    print("Opened database successfully in the restrictions route")
+    cur = conn.cursor()
+
+    sqlQ = ("""SELECT * FROM LakelandEatsList WHERE rowid=2;""")
+    print(sqlQ)
+    cur.execute(sqlQ)
+    data = cur.fetchall()
+
+    return render_template('abuelos-mexican.html', data=data)
+
 
 
 @app.route('/login')
@@ -87,7 +138,7 @@ def add_new_restaurant():
             )
             qr.add_data(f'http://127.0.0.1:5000/{name}')
             qr.make(fit=True)
-            img = qr.make_image(fill_color="purple", back_color="black")
+            img = qr.make_image(fill_color='#532d8e', back_color="black")
             img.save(f"./static/images/qrcodes/qr{name}.png")
 
             return redirect(url_for('database'))
@@ -97,6 +148,39 @@ def add_new_restaurant():
 
     else:
         return render_template('/add-new-restaurant.html')
+    
+
+@app.route('/edi-restaurant/<string:id>', methods=["GET", "POST"])
+def edit_restaurant(id):
+    #Create cursor
+    cur = conn.cursor()
+
+    #Get restaurant by id
+    result = cur.execute("SELECT * FROM LakelandEatsList WHERE id = ?", [id])
+    restaurant = result.fetchone()
+
+    # https://www.youtube.com/watch?v=Us9DuF4KWUE
+
+    if request.method == "POST":
+        try:
+            name = request.form['name']
+            location = request.form['location']
+            distance = request.form['distance']
+            price = request.form['price']
+            review = request.form['review']
+
+            with sql.connect("SSEData.db") as con:
+                cur = con.cursor()
+                cur.execute("UPDATE LakelandEatsList SET Location=?, Distance=?, Price=?, Review=? WHERE Name=?", (location, distance, price, review, name))
+                con.commit()
+                msg = "Record successfully updated"
+            return redirect(url_for('database'))
+        except:
+            con.rollback()
+            msg = "error in update operation"
+
+    else:
+        return render_template('/edit-restaurant.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
